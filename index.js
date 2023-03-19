ee7431e79d42adc45bae85a867df3281c5b6796b
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-03-19 22:33:31 +0100

    sidebar effect

---
+++ index.js
@@ -0,0 +1,17 @@
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : id > classe > baliseHtml
+
+
+const btn = document.querySelector('.toggle-btn')
+const menu = document.querySelector('#side-bar');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
